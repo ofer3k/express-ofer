ed6177429c5029bfcc331e0bfeb7cfd467d73fd1
ofer3k <ofer3klein@gmail.com>
2021-11-02 17:33:33 +0200

    asd

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.get("/data", (req, res) => {
+  res.send({ name: "ofer" });
+});
+app.get("/dataJson", (req, res) => {
+  res.send(JSON.stringify({ name: "ofer" }));
 });
 app.listen(port, () => {
   console.log(`Running on ${port}`);
